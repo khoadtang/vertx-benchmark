355d405f65816b7e96687b5281289f925e7e403c
khoadtang <khoa.tangduy@gmail.com>
2023-09-04 09:54:10 +0700

    add load testing script

---
+++ go-benchmark/loadtest/script.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '1m', target: 1000 } // Ramp up to 1000 RPS over 1 minute
+  ],
+};
+
+export default function () {
+  // Send a GET request to the specified endpoint
+  const response = http.get('http://localhost:8080/go/fetch');
+
+  // Print the response status to the console
+  console.log(`Response status: ${response.status}`);
+
+  // Sleep for a random duration between 0.5s and 1s
+  sleep(Math.random() * (1 - 0.5) + 0.5);
+}
